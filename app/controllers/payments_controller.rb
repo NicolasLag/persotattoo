ac39ed45dfae38b179c868fd701698525cc6d858
louis pons <louis.pons@icloud.com>
2016-03-08 16:15:36 +0100

    stripe implemented

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -22,7 +22,7 @@ class PaymentsController < ApplicationController
 
     @order.update(payment: charge.to_json, state: 'paid')
 
-    redirect_to order_path(@order)
+    redirect_to me_path
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
